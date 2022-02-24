edc69fef412339d5cb4be56f14a57d9616a984fa
DMITRY PRAKHT <98157604+Prakht-Dm@users.noreply.github.com>
2022-02-24 23:09:40 +0700

    no need

--- view.js
+++
@@ -1,9 +0,0 @@
-let crossButton = querySelectorAll('input.cross');
-
-let id=5;
-export function createHigh{
-let outTask = document.createElement('div');
-outTask.className='activeTask';
-div.innerHTML=`<div class="checkBox"><input type="checkbox" id="r${id}"><label for="r${id}"><span></span></label></div><p>${valueTask}</p><button class="cross2">`
-HITHTask.append(div)
-}
